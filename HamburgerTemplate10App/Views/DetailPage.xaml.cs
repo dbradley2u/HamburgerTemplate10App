327e829ca2eb944666309cf7d5afe324223799d0
Denise <Denise@DENISE-PC>
2016-05-17 23:27:46 -0400

    Add project files.

---
+++ HamburgerTemplate10App/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using HamburgerTemplate10App.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace HamburgerTemplate10App.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
